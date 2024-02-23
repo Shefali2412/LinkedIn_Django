0bf7d266b9db2d1a41f35b88de6e29978aab950c
Shefali Johnson <148550787+Shefali2412@users.noreply.github.com>
2024-02-23 15:25:40 +0100

    Update urls.py

--- linkedin/myapp/urls.py
+++ linkedin/myapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
-    path('member/', views.member_list, name='member_list'),  # Updated URL for member_list
+    path('member/', views.member_list, name='member_list'),  
     path('profile/<str:username>/', views.view_profile, name='view_profile'),
 ]
